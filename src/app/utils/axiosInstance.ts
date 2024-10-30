82e5fce7aa92c8fc15907984cbf889910f92850f
Binaryy <ibolarinwa606@gmail.com>
2024-10-30 11:02:19 +0100

    updated axiosinstance

--- src/app/utils/axiosInstance.ts
+++ src/app/utils/axiosInstance.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', // Replace with your FastAPI server URL
+  baseURL: 'https://fast-backend-n.onrender.com', // Replace with your FastAPI server URL
   headers: {
     'Content-Type': 'application/json',
   },
