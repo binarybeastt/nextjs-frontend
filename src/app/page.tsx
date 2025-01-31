c75de40d0ae9e849e24f4a99785aa73b62de1ad4
Oladeji <olawunioladeji06@gmail.com>
2025-01-31 10:43:00 +0100

    added new themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
-// import React, { useState } from "react";
 import Link from "next/link";
 import SignUp from "@/app/components/SignUp";
-import Modal from '@/app/components/Modal'
+import Modal from "@/app/components/Modal";
+import Modes from "@/app/components/lightDarkModes"
+
+
 // import useState from 'react'
 
 const page = () => {
@@ -10,6 +12,7 @@ const page = () => {
     <>
       <div className="px-4 py-4 sm:px-0 sm:py-0">
         <div className="sm:bg-[#E9F9FF] mt-10 sm:py-10 sm:mt-0 sm:pb-28 relative">
+          <Modes />
           <p className="border-2 sm:bg-white sm:border sm:w-[70%] sm:mt-10 sm:mx-auto  sm:py-1 rounded-[10px]  px-5 font-bold text-[30px] capitalize">
             deji___ <br />
             school
@@ -29,16 +32,26 @@ const page = () => {
           </div>
         </div>
         {/* mobile screen sign up component */}
-        <div className="hidden sm:flex absolute left-[780px] top-[240px] ">
+        <div className="hidden sm:flex absolute left-[730px] top-[240px] ">
           <SignUp />
         </div>
 
         <div className="sm:mx-20 sm:absolute sm:top-[350px] sm:left-10">
-          <Modal/>
+          <Modal />
         </div>
         <div className="flex sm:hidden px-5 py-2 mt-10 justify-between pb-20">
-          <Link className="text-white bg-[#3056EC] px-5 py-2 rounded-[10px]" href={'Signup'}>sign up</Link>
-          <Link className="text-white bg-[#3056EC] px-5 py-2 rounded-[10px]" href={'login'}>Log In</Link>
+          <Link
+            className="text-white bg-[#3056EC] px-5 py-2 rounded-[10px]"
+            href={"Signup"}
+          >
+            sign up
+          </Link>
+          <Link
+            className="text-white bg-[#3056EC] px-5 py-2 rounded-[10px]"
+            href={"login"}
+          >
+            Log In
+          </Link>
         </div>
       </div>
     </>
